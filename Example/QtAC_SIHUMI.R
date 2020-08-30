f5f02ab1b4090ceb8c5716dbf3abd2203f8dbe28
Hannah Schrenk <hannah-schrenk@arcor.de>
2020-08-30 15:34:33 +0200

    add code and files

---
+++ Example/QtAC_SIHUMI.R
@@ -0,0 +1,53 @@
+#---------------------------------------------------------------------------------------------#
+#                            === EXAMPLE SIHUMI ===
+#---------------------------------------------------------------------------------------------#
+# Author: Nico Schreiber and Hannah Schrenk
+# Version: 1.0
+#---------------------------------------------------------------------------------------------#
+# INITIALIZATION
+
+work_folder  <- "D/Users/..."                           # folder containing the abundance data
+observ_data  <- "QtAC_SIHUMI.txt"                       # file containing the abundance data
+infodyn_path <- " D/Users/.../dist/MTinfodynamics.jar"  # path of the MTinfodynamics.jar file (included in "dist")
+setwd(work_folder)
+
+num_timepoints <- 30   # length of the time windows serving as basis for the transfer entropy calculations
+signfac        <- 0.1  # significance level
+
+#---------------------------------------------------------------------------------------------#
+# CALCULATIONS
+
+# load the data into the workspace
+
+Data <- QtAC.TXT.reader(observ_data,col_names=FALSE,row_names = TRUE)
+
+# compute networks of information transfer for every time point starting from num_timepoints
+
+result_mtx <- QtAC(Data,num_timepoints, JavaPath = infodyn_path, l = 10L, k = 10L, delay = 2L)
+
+# take only information transfers passing the significance level into account
+
+result_mtx_sig <- QtAC.Signfactor(result_mtx,signfac)
+
+# calculate the three systemic variables for every network
+
+maturation <- QtAC.maturation(result_mtx_sig)
+
+#----------------------------------------------------------------------------------------------#
+# VISUALIZATIONS
+
+# plot the first network of information transfers (corresponding to time point 30) and save it
+
+QtAC.network(result_mtx_sig,num_mtx = 1,edge_label = TRUE, layout = "nicely", Save = TRUE)
+
+# plot the development of potential, connectedness, and resilience over time and save it
+
+QtAC.2dplot(maturation, Save = TRUE)
+
+# plot the development of potential and connectedness w.r.t. each other
+
+QtAC.2dmixplot(maturation, "potential", "connectedness")
+
+# plot a 3D plot of potential, connectedness, and resilience
+
+QtAC.3dplot(maturation,Mat_points = TRUE)
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# QtAC
- Quantifying the Adaptive Cycle
+---Package QtAC---
+
+
+ESSENTIAL SOFTWARE:
+
+- R		(https://www.r-project.org/)
+- Perl		(https://www.perl.org/get.html)
+
+--------------------------------
+
+INSTALLING PROCESS:
+
+ ===(R Studio)===
+
+1) Tools -> Install Packages...
+
+2) -> Install from: Package Archive File
+   -> Browse: Choose "QtAC_1.0.tar.gz"
+   -> Install
+
+3) Type library(QtAC)
+
+ ===Command===
+
+1) > install.packages("D:/.../QtAC_1.0.tar.gz", repos = NULL, type = "source")
+2) > library(QtAC)
+
+--------------------------------
+
+SUPPLEMENTARY MATERIAL:
+
+The QtAC folder contains a manual "QtAC_0.1.0.pdf" describing the main functions. An example application to a bacterial community can be found in the subfolder "Example". The subfolder "dist" contains the file "MTinfodynamics.jar" which is necessary to run the main function QtAC.  
+
+
